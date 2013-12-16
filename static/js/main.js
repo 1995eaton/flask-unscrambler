11bc5c631161e6bdbe074efe2296df00d7586b7e
1995eaton <1995eaton@gmail.com>
2013-12-16 00:59:27 -0500

    upd

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-	$(".wordnew").animate({'top': '+=30'}, 200);
+	document.form.inputAnagrams.focus();
+	$('.githubLogo').click( function() {
+		window.location='https://github.com/1995eaton/flask-unscrambler';
+	});
+	$(".listAnagrams").animate({'top': '+=30'}, 200);
 	$("#infoButton").click(function() {
 		$("#infoButton").animate({"right": "-=25"}, 500);
-		$("#infoBox").animate({'right': '+=177'}, 500);
+		$("#infoBox").animate({'right': '+=192'}, 500);
 	});
-	$(".ibCloseButton").click(function() {
+	$('.closeBox,.inputBox').click(function() {
 		$("#infoButton").animate({"right": "+=25"}, 500);
-		$("#infoBox").animate({"right": "-=177"}, 500);
-	});
-	$(".closeBox").click(function() {
-		$("#infoButton").animate({"right": "+=25"}, 500);
-		$("#infoBox").animate({"right": "-=177"}, 500);
+		$("#infoBox").animate({"right": "-=192"}, 500);
 	});
 });
--- unscrambler.py
+++ unscrambler.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, render_template
-from markupsafe import Markup
 import os, csv
+
 app = Flask(__name__)
+
 dictionary = {key: value for key, value in csv.reader(open("dictionary"))}
+
 @app.route('/', methods=['GET', 'POST'])
-def main():
-    if request.method == 'GET':
-        return render_template('index.html')
-    if request.method == 'POST':
-        anagram_input = ''.join([i for i in request.form['anagram_input'].lower() if i.isalpha()])
-        try:
-            r = dictionary[''.join(sorted(anagram_input))]
-        except KeyError:
-            return render_template('index.html', previous_anagrams = Markup(request.form['previous_anagrams']))
-        return render_template('index.html', current_anagrams = Markup(r.replace(",", "<br>")), previous_anagrams = Markup(request.form['previous_anagrams']))
+def unscrambleWord():
+	if request.method == 'GET':
+		return render_template('index.html')
+	if request.method == 'POST':
+		try:
+			inputAnagrams = dictionary[''.join(sorted(''.join([i for i in request.form['inputAnagrams'].lower() if i.isalpha()])))]
+		except KeyError:
+			return render_template('index.html', anagrams = request.form['previousAnagrams'], static = True)
+		if request.form['previousAnagrams'] == '':
+			return render_template('index.html', anagrams = inputAnagrams.replace(',', ', '), static = False)
+		return render_template('index.html', anagrams = inputAnagrams.replace(',', ', ') + "<hr>" + request.form['previousAnagrams'], static = False)
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
