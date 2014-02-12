1052721f842cdb3764cdcb1f98960d812ab561f1
1995eaton <1995eaton@gmail.com>
2014-02-12 01:28:01 -0500

    Indentation

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,19 @@
 var animationSpeed = 300;
 
 $(document).ready(function() {
-	document.form.inputAnagrams.focus();
-	$('.githubLogo').click( function() {
-		window.location='https://github.com/1995eaton/flask-unscrambler';
-	});
-	$(".listAnagrams").animate({'top': '+=30'}, 200);
-	$("#infoButton").click(function() {
-		$("#infoButton").animate({"right": "-=25"}, animationSpeed);
-		$("#infoBox").animate({'right': '+=192'}, animationSpeed);
-	});
-	$('.closeBox,.inputBox').click(function() {
-      if ($("#infoBox").position().left != window.innerWidth) {
-         $("#infoButton").animate({"right": "+=25"}, animationSpeed);
-         $("#infoBox").animate({"right": "-=192"}, animationSpeed);
-      }
-	});
+  document.form.inputAnagrams.focus();
+  $('.githubLogo').click( function() {
+    window.location='https://github.com/1995eaton/flask-unscrambler';
+  });
+  $(".listAnagrams").animate({'top': '+=30'}, 200);
+  $("#infoButton").click(function() {
+    $("#infoButton").animate({"right": "-=25"}, animationSpeed);
+    $("#infoBox").animate({'right': '+=192'}, animationSpeed);
+  });
+  $('.closeBox,.inputBox').click(function() {
+    if ($("#infoBox").position().left != window.innerWidth) {
+      $("#infoButton").animate({"right": "+=25"}, animationSpeed);
+      $("#infoBox").animate({"right": "-=192"}, animationSpeed);
+    }
+  });
 });
